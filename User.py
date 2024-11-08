61234fe131d239787adda9491d71d6e6c1296505
gusenga thierry <t.gusenga@alustudent.com>
2024-11-08 19:34:37 +0200

    ok

--- User.py
+++ User.py
@@ -4,3 +4,10 @@ class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
+
+class Hotel:
+    def __init__(self, name, location, amenities, room_types):
+        self.name = name
+        self.location = location
+        self.amenities = amenities
+        self.room_types = room_types
