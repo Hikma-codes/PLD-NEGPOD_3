bed4310a8b3c792863b9278fbd7d99d78e2915ce
gusenga thierry <t.gusenga@alustudent.com>
2024-11-08 20:00:24 +0200

    hotel .py

--- User.py
+++ User.py
@@ -48,9 +48,4 @@ class User:
         else:
             print("No user found with this email. Please sign up first.")
 
-class Hotel:
-    def __init__(self, name, location, amenities, room_types):
-        self.name = name
-        self.location = location
-        self.amenities = amenities
-        self.room_types = room_types
+
---
+++ hotel.py
@@ -0,0 +1,6 @@
+class Hotel:
+    def __init__(self, name, location, amenities, room_types):
+        self.name = name
+        self.location = location
+        self.amenities = amenities
+        self.room_types = room_types
